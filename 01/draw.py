2a9b4a16aae11bd8bb3c83f59bbe5ac7a761724b
AdupaNithinSai <adupa@umich.edu>
2024-09-29 20:19:27 -0400

    Add files via upload

---
+++ 01/draw.py
@@ -0,0 +1,4 @@
+def draw_game(result):
+    print("=== Game Visualization: Pattern Recognition and Neural Networks ===")
+    print(result)
+    print("=========================================================")
