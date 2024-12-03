b3a57869db635ba5e014bdabf024124790c154ae
AdupaNithinSai <adupa@umich.edu>
2024-12-02 22:56:38 -0500

    push

--- 10/dogs_cats.py
+++ 10/dogs_cats.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e07136665a07f12809c9c1ca40adf710c165ac62b5f75d462b523befa37bfe78
-size 5521
+oid sha256:38c343cd105d80f64a6e510df41904801029d5ef6555e0b171954c15a0c86643
+size 5359
